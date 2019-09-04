5b6d17f77bbfc3da651d1c368ca51988cc993a59
BW05Genius <1061668182@qq.com>
2019-09-04 19:55:32 +0800

    二代版本

---
+++ src/main/java/com/bw/bean/Person.java
@@ -0,0 +1,29 @@
+package com.bw.bean;
+/** 
+* @author 作者:王澍
+* @version 创建时间：2019年9月4日 下午7:54:32  
+*/
+public class Person {
+	private int pid;
+	private String pname;
+	private String date;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
